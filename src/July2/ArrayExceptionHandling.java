cfb7aa4ee2a037a628212a1f02e41ff6cb11e7b9
pranshigupta94 <pranshigupta94@gmail.com>
2024-07-10 18:16:16 -0400

    July 9

---
+++ src/July2/ArrayExceptionHandling.java
@@ -0,0 +1,20 @@
+package July2;
+
+public class ArrayExceptionHandling {
+    public static void main(String[] args) {
+
+        int[] arr = new int[5];
+        try {
+            arr = new int[]{1,2,3,4,5,6,7};
+
+            for (int i = 0; i < arr.length; i++){
+                System.out.println(arr[i]);
+            }}
+            catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(e);
+                e.printStackTrace();
+            }
+        System.out.println("Testing");
+
+    }
+}
---
+++ src/July2/Demo.java
@@ -0,0 +1,16 @@
+package July2;
+
+public class Demo {
+    public static void main(String[] args) {
+        String s = "Prgara";
+        String uppercase = s.toUpperCase();
+        System.out.println(uppercase);
+
+        String s1 = null;
+        System.out.println(s1);
+        uppercase = s1.toUpperCase(); // exception occur here because of operation perform on null
+        System.out.println(uppercase);
+
+
+    }
+}
